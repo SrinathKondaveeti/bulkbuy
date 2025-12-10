001c362bf823549675fae8b309ab02893dbc5a63
Srinath Kondaveeti <kondaveetisrinath@gmail.com>
2025-12-11 00:49:45 +0530

    Cross

--- src/main/java/com/bulkbuy/config/WebConfig.java
+++ src/main/java/com/bulkbuy/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                // .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://srinathkondaveeti.github.io")
+                .allowedOrigins("https://srinathkondaveeti.github.io/bulkly/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)                     // set false if allowedOrigins="*"
