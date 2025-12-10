3cdeaefad6109f2bf7d78984e8a322eee13f2f76
Srinath Kondaveeti <kondaveetisrinath@gmail.com>
2025-12-11 01:54:14 +0530

    Filter for cross issue

--- src/main/java/com/bulkbuy/filter/CorsFilterConfig.java
+++ src/main/java/com/bulkbuy/filter/CorsFilterConfig.java
@@ -1,7 +1,9 @@
 package com.bulkbuy.filter;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -9,19 +11,37 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsFilterConfig {
 
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        System.out.println("$$$$$$$$ CorsFilter executed $$$$$$$$");
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("https://srinathkondaveeti.github.io/bulkly"); // or config.addAllowedOriginPattern("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
         System.out.println("$$$$$$$$ CorsFilter executed $$$$$$$$");
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://srinathkondaveeti.github.io/bulkly"); // or config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("https://srinathkondaveeti.github.io/bulkly");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        CorsFilter corsFilter = new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 }
