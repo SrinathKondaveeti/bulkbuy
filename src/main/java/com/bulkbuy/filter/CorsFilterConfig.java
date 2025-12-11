f6b13dd693eea4428d7686702fdaf8e9d0b88d26
Srinath Kondaveeti <kondaveetisrinath@gmail.com>
2025-12-11 10:27:07 +0530

    Cross config

--- src/main/java/com/bulkbuy/filter/CorsFilterConfig.java
+++ src/main/java/com/bulkbuy/filter/CorsFilterConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsFilterConfig {
@@ -26,22 +29,39 @@ public class CorsFilterConfig {
 //        return new CorsFilter(source);
 //    }
 
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+//        System.out.println("$$$$$$$$ CorsFilter executed $$$$$$$$");
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("https://srinathkondaveeti.github.io/bulkly");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        CorsFilter corsFilter = new CorsFilter(source);
+//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
+//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return bean;
+//    }
+
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
-        System.out.println("$$$$$$$$ CorsFilter executed $$$$$$$$");
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://srinathkondaveeti.github.io/bulkly");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
+    public CorsConfigurationSource corsConfigurationSource() {
+        System.out.println("@#$$$$$$$$$$$$$$$$$$ SecurityConfig CorsConfiguration entered $$$$$$$$$$$$$$$$$$$$$$$#@");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://srinathkondaveeti.github.io/bulkly"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L); // How long the pre-flight request can be cached
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        CorsFilter corsFilter = new CorsFilter(source);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
